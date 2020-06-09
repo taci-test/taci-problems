5171edb95da599f647fe05c34128906422edd281
Taci Shlosberg <taciichka@gmail.com>
2020-06-09 15:46:03 -0700

    count zeroes

---
+++ count-zeroes.js
@@ -0,0 +1,13 @@
+function countZeroes(arr) {
+    
+    var output = 0
+
+    for (var i = 0; i < arr.length; i++) {
+
+    }
+    return output;
+}
+console.log(countZeroes([0,1,0,1,1,0,0,1])) // 2
+console.log(countZeroes([1,1,1,1,0,1])) // 4
+console.log(countZeroes([1,1,0,1,0,1,1,1])) // 3
+console.log(countZeroes([0,0,0])) // 0
